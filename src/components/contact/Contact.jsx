c81e0ee668ac0a337f22f26eb50cbef286f923f7
HamedMiri72 <hamedmiri72@gmail.com>
2025-02-01 13:10:13 +0000

    menu button

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -3,7 +3,7 @@ import "./Contact.scss"
 
 const Contact = () => {
   return (
-    <div className='contact' id=''></div>
+    <div className='contact' id='contact'></div>
   )
 }
 
--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,9 +1,46 @@
 import React from 'react'
 import "./Topbar.scss"
+import EmailIcon from '@mui/icons-material/Email';
+import PersonIcon from '@mui/icons-material/Person';
 
-const Topbar = () => {
+const Topbar = ({menuOpen, setMenuOpen}) => {
   return (
-    <div className='topbar' id='topbar'></div>
+    <div className= {"topbar " + (menuOpen && "active")} id='topbar'>
+
+      <div className="wrapper">
+
+        <div className="left">
+            <a href="#intro" className='logo'>genius.</a>
+
+            <div className="itemContainer">
+
+              <PersonIcon className='icon'/>
+              <span>+44 735 584 8222</span>
+
+            </div>
+
+            <div className="itemContainer">
+
+              <EmailIcon  className='icon'/>
+              <span>hamedmiri72@gmail.com</span>
+
+            </div>
+        </div>
+
+        <div className="right">
+
+          <div className="humberguer" onClick={() => setMenuOpen(!menuOpen)}>
+
+            <span className='line-1'></span>
+            <span className='line-2'></span>
+            <span className='line-3'></span>
+          </div>
+
+        </div>
+
+      </div>
+
+    </div>
   )
 }
 
